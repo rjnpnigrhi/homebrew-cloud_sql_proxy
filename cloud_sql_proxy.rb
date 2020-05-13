3917929d73957c082e04803a53389d9e1902eaae
Rajan Panigrahi <rjnpnigrhi@gmail.com>
2020-05-13 11:49:46 +0530

    Update cloud_sql_proxy.rb

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,19 +1,18 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
-  homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "1.17"
-  url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/#{version}.tar.gz"
+  homepage "https://github.com/rjnpnigrhi/homebrew-cloud_sql_proxy"
+  url "https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64"
   sha256 "cd4f426f779ede8c87f0efd7f47b7fd86f59157192df37a62822dd9ff53e5113"
-  head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
+  head "https://github.com/rjnpnigrhi/homebrew-cloud_sql_proxy.git"
   
   depends_on "go" => :build
   depends_on "git" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/GoogleCloudPlatform/cloud_sql_proxy").install buildpath.children
+    (buildpath/"src/github.com/rjnpnigrhi/homebrew-cloud_sql_proxy").install buildpath.children
     
-    cd "src/github.com/GoogleCloudPlatform/cloud_sql_proxy/cmd/cloud_sql_proxy" do
+    cd "src/github.com/rjnpnigrhi/homebrew-cloud_sql_proxy/cmd/cloud_sql_proxy" do
       system "go get"
       system "go build"
       system "mkdir bin"
